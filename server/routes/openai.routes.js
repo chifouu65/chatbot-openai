1c734160ba506b95e51d02e5108e20d86f2476a5
Noah <89213023+chifouu65@users.noreply.github.com>
2023-04-08 17:25:21 +0200

    .

--- server/routes/openai.routes.js
+++ server/routes/openai.routes.js
@@ -15,12 +15,12 @@ const openai = new OpenAIApi(config);
 
 const fetchProducts = async () => {
     try {
-        const response = await fetch('https://api.pierrette-essentielle.com/api/products?populate=*&[filters][categories][id]=2&[filters][price][$lte]=50&sort=price'
+        const response = await fetch(process.env.API_URL + '/products?populate=*&[filters][categories][id]=2&[filters][price][$lte]=50&sort=price'
         ,{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'bearer 4dcc75be011d7d3b82cd71c8219728b6cac10d3987b40ae8c6e84a4743de3b4918ee4980535d9c582b5e1d8400df88f9946204668a912724031c1ba0553a8a9e40c48296be4f8205bfa17449c03d2bfb271accca12871b316d968b9fe71733fba773c96549c6ea70c347c077e8c1abcf20a76b3e1073fdd3f4c71c6dfbdd0ac3'
+                'Authorization': 'bearer ' + process.env.API_KEY
                 }
             }
         ).then((response) => {
@@ -103,8 +103,7 @@ router.route('/').post(async (req, res) => {
                 message: "Please enter a text with less than 300 characters",
             }
         });
-        return;
-    }
+        return;}
 
     try {
         const response = await openai.createCompletion({
